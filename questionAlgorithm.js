1792b5b67c1ed26c4c01ea762f5d772a8441c428
sportymat <mark.tarrant7@gmail.com>
2019-01-02 15:39:47 +0000

    question state object added and logic added to change state

--- questionAlgorithm.js
+++ questionAlgorithm.js
@@ -27,16 +27,28 @@ student.prototype = {
         6: 1, 
         7: 1, 
         8: 1
+    }, 
+
+    questionState: { 
+        1: 0, 
+        2: 0, 
+        3: 0, 
+        4: 0, 
+        5: 0, 
+        6: 0, 
+        7: 0, 
+        8: 0
     }
 };
 
 let markTarrant = new student("Mark Tarrant");
 
-
 //algorithm that creates an array from which future questions can be selected
 function curriculumArray(obj) {
     let array = []; 
+    //creates a demo object to make sure the original object is not changed
     let demoObject = obj; 
+    //iterates through each key in the student.curriculum object and creates an array based on the value assigned to each key
     for (var property in demoObject) {
         while (demoObject[property] > 0) {
             array.push(property); 
@@ -46,7 +58,7 @@ function curriculumArray(obj) {
     return array; 
 }
 
-//uses the above array to select a catory number which can then be looked up using the curriculum object above
+//uses the above array to select a category number which can then be looked up using the curriculum object above
 function categorySelector(arr) {
     let randomIndex = Number((Math.random() * arr.length).toFixed());
     return arr[randomIndex];
@@ -58,39 +70,37 @@ let category = curriculumObject[categoryKey];
 //category is the next category to be questioned, can then use this as the key to select the next question from the question array
 console.log(category);
 
-
-
 let questionObject = {
-    "Number and Place Value": {
+    1: {
         1: "What is 10 multiplied by 3", 
         2: "What is 6 multiplied by 7"
     },
 
-    "Calculating": {
+    2: {
 
     }, 
 
-    "Fractions, Decimals and Percentages": {
+    3: {
 
     }, 
 
-    "Ratio and Proportion": {
+    4: {
 
     }, 
 
-    "Algebra": {
+    5: {
 
     }, 
 
-    "Measurement": {
+    6: {
 
     }, 
 
-    "Geometry": {
+    7: {
 
     }, 
 
-    "Statistics": {
+    8: {
 
     }
 
@@ -98,36 +108,36 @@ let questionObject = {
 };
 
 let answerObject = {
-    "Number and Place Value": {
+    1: {
         1: 30, 
         2: 42
     },
 
-    "Calculating": {
+    2: {
 
     }, 
 
-    "Fractions, Decimals and Percentages": {
+    3: {
 
     }, 
 
-    "Ratio and Proportion": {
+    4: {
 
     }, 
 
-    "Algebra": {
+    5: {
 
     }, 
 
-    "Measurement": {
+    6: {
 
     }, 
 
-    "Geometry": {
+    7: {
 
     }, 
 
-    "Statistics": {
+    8: {
         
     }
 };
@@ -135,6 +145,8 @@ let answerObject = {
 
 
 //To do list 
+//questionState function is very specific, needs to know what the question is, maybe need to write the question algorithm first
+//may need to write extensive tests as this re-sets every time
 //need to turn each question object category into an array as opposed to further objects, can then just loop through them and surface a random question
 //similar for the answer object
 //need to dynamically update the student object based on whether the student answered the question correctly or not
@@ -143,7 +155,14 @@ let answerObject = {
 let kathrynWiltshire = new student("Kathryn Wiltshire"); 
 
 //user input value stored
-let answer = 30;
-let question;
+let studentAnswer = 30;
+let question = questionObject[1][1];
+
+//change question state based on answer of question
+function questionState() {
+    if(question == "What is 10 multiplied by 3" && studentAnswer !== 30) {
+        markTarrant.questionState[1] += 1; 
+    };
+}
 
 
